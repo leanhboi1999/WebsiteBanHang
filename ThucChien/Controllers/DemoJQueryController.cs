be63b7f726cf6cae1683dc69963f5934616b8203
Lê Đúc Anh <leanhboi1999@outlook.com>
2020-04-19 15:48:48 +0700

    Add project files.

---
+++ ThucChien/Controllers/DemoJQueryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThucChien.Controllers
+{
+    public class DemoJQueryController : Controller
+    {
+        // GET: DemoJQuery
+        public ActionResult Demo()
+        {
+            return View();
+        }
+
+        /*public ActionResult TestMethodJquery()
+        {
+            return View();
+        }*/
+    }
+}
---
+++ ThucChien/Controllers/KhachHangController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThucChien.Models;
+
+namespace ThucChien.Controllers
+{
+    
+    public class KhachHangController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        public ActionResult Index()
+        {
+            //Cach 1:
+            //var lstKH = from KH in db.KhachHangs select KH;
+            //Cach 2:
+            var lstKH = db.KhachHangs;
+            return View(lstKH);
+        }
+        public ActionResult Index1()
+        {
+            var lstKH = from KH in db.KhachHangs select KH;
+            return View(lstKH);
+        }
+
+        public ActionResult TruyVan1DoiTuong()
+        {
+            //Cach1: Cau truy van
+            //var lstKH = from KH in db.KhachHangs where KH.MaKH == 2  select KH;
+            //KhachHang khang = new KhachHang();
+            //khang = lstKH.FirstOrDefault();
+            //Cach 2: Phuong thuc ho tro
+            KhachHang khang = db.KhachHangs.SingleOrDefault(n => n.MaKH == 2);
+            return View(khang);
+        }
+        public ActionResult SortDuLieu()
+        {
+            List<KhachHang> lstKh = db.KhachHangs.OrderByDescending(n => n.TenKH).ToList();
+            return View(lstKh);
+        }
+        public ActionResult GroupDuLieu()
+        {
+            List<ThanhVien> lstTV = new List<ThanhVien>();
+            lstTV = db.ThanhViens.OrderByDescending(n => n.TaiKhoan).ToList();
+            return View(lstTV);
+        }
+        /*protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                    db.Dispose();
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }*/
+    }
+
+}
