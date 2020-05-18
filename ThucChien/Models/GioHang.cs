ef3c9a6d6324f9b4a92729e4140aa212690d9ad5
Lê Đúc Anh <leanhboi1999@outlook.com>
2020-05-18 09:58:56 +0700

    Thêm class metadata lưu thay đổi của model, thêm ViewModel giỏ hàng

---
+++ ThucChien/Models/GioHang.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThucChien.Models
+{
+    public class GioHang
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public int SoLuong { get; set; }
+        public decimal DonGia { get; set; }
+        public decimal ThanhTien { get; set; }
+        public string HinhAnh { get; set; }
+
+        public GioHang(int iMaSP, int soluong)
+        {
+            using (QuanLyBanHangEntities db = new QuanLyBanHangEntities())
+            {
+                this.MaSP = iMaSP;
+                SanPham sp = db.SanPhams.Single(n => n.MaSP == iMaSP);
+                this.TenSP = sp.TenSP;
+                this.HinhAnh = sp.HinhAnh;
+                this.DonGia = sp.DonGia.Value;
+                this.SoLuong = soluong;
+                this.ThanhTien = DonGia* SoLuong;
+            }
+        }
+
+        public GioHang()
+        {
+
+        }
+    }
+}
---
+++ ThucChien/Models/Metadata/ThanhVien.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ThucChien.Models
+{
+    [MetadataTypeAttribute(typeof(ThanhVienMetadata))]
+    public partial class ThanhVien
+    {
+        internal sealed class ThanhVienMetadata
+        {
+            //Danh sách các thuộc tính
+            [DisplayName("Mã thành viên")]
+            public int MaThanhVien { get; set; }
+            [DisplayName("Tài khoản")]
+            [Required(ErrorMessage = "</br>Nhập tài khoản")]
+            [StringLength(10, ErrorMessage = "Không quá 10 kí tự")]
+            //[Range(5, 10 , ErrorMessage)]
+            public string TaiKhoan { get; set; }
+            [DisplayName("Mật khẩu")]
+            public string MatKhau { get; set; }
+            [DisplayName("Họ tên")]
+            public string HoTen { get; set; }
+            [DisplayName("Địa chỉ")]
+            public string DiaChi { get; set; }
+            [DisplayName("Email")]
+            public string Email { get; set; }
+            [DisplayName("Số điện thoại")]
+            public string SoDienThoai { get; set; }
+            [DisplayName("Câu hỏi")]
+            public string CauHoi { get; set; }
+            [DisplayName("Câu trả lời")]
+            public string CauTraLoi { get; set; }
+            [DisplayName("Mã loại thành viên")]
+            public Nullable<int> MaLoaiTV { get; set; }
+        }
+    }
+}
