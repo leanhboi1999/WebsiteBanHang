afa7a1889495ddbff934516b2581460bd83b7e56
Lê Đúc Anh <leanhboi1999@outlook.com>
2020-05-18 22:40:21 +0700

    Bước đầu khởi tạo giỏ hàng

---
+++ ThucChien/Controllers/GioHangController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThucChien.Models;
+
+namespace ThucChien.Controllers
+{
+    public class GioHangController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        //Get data giỏ hàng
+        public List<GioHang> LayGioHang()
+        {
+            //Giỏ hàng đã tồn tại
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (Session["GioHang"] == null)
+            {
+                //Không tồn tại session
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        } 
+
+        //Thêm giỏ hàng dùng reload, không dùng ajax
+        public ActionResult ThemGioHang(int MaSP, string strURL)
+        {
+            //Kiểm tra sản phẩm có tồn tại hay không
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == MaSP);
+            return Json("test");
+        }
+
+
+        // GET: GioHang
+        public ActionResult XemGioHang()
+        {
+            return View();
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            return PartialView();
+        }
+    }
+}
