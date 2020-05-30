130dbcf3a93b118fbd2fc27154b08da1ccc64739
Lê Đúc Anh <leanhboi1999@outlook.com>
2020-05-30 20:34:05 +0700

    Hoàn thành giỏ hàng
    Làm giỏ hàng lưu trữ bằng session

--- ThucChien/Controllers/GioHangController.cs
+++ ThucChien/Controllers/GioHangController.cs
@@ -11,14 +11,14 @@ namespace ThucChien.Controllers
     {
         QuanLyBanHangEntities db = new QuanLyBanHangEntities();
         //Get data giỏ hàng
-        public List<GioHang> LayGioHang()
+        public List<ItemGioHang> LayGioHang()
         {
             //Giỏ hàng đã tồn tại
-            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
             if (Session["GioHang"] == null)
             {
                 //Không tồn tại session
-                lstGioHang = new List<GioHang>();
+                lstGioHang = new List<ItemGioHang>();
                 Session["GioHang"] = lstGioHang;
             }
             return lstGioHang;
@@ -29,18 +29,80 @@ namespace ThucChien.Controllers
         {
             //Kiểm tra sản phẩm có tồn tại hay không
             SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == MaSP);
-            return Json("test");
+            if (sp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Tạo list giỏ hàng (thực ra là lấy giỏ hàng ra ngoài)
+            List<ItemGioHang> lstGioHang = LayGioHang();
+            //Trường hợp sản phẩm đã tồn tại trong giỏ hàng
+            ItemGioHang spCheck = lstGioHang.SingleOrDefault(n => n.MaSP == MaSP);
+            if (spCheck != null)
+            {
+                if (spCheck.SoLuong > sp.SoLuongTon)
+                {
+                    return View("ThongBao");
+                }
+                //Tăng số lượng và tính lại giá trị tiền
+                spCheck.SoLuong++ ;
+                spCheck.ThanhTien = spCheck.SoLuong * spCheck.DonGia;
+                return Redirect(strURL);
+            }
+            ItemGioHang itemGH = new ItemGioHang(MaSP);
+            if (itemGH.SoLuong > sp.SoLuongTon)
+            {
+                return View("ThongBao");
+            }
+            lstGioHang.Add(itemGH);
+            return Redirect(strURL);
         }
 
+        public double TinhTongSoLuong()
+        {
+            //Lấy giỏ hàng ra ngoài
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
+            if (lstGioHang == null)
+            {
+                return 0;
+            }
 
+            return lstGioHang.Sum(n => n.SoLuong);
+
+        }
+        //Tính tổng tiền của giỏ hàng
+        public decimal TinhTongTien()
+        {
+            //Lấy giỏ hàng ra ngoài
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
+            if (lstGioHang == null)
+            {
+                return 0;
+            }
+
+            return lstGioHang.Sum(n => n.ThanhTien);
+        }
         // GET: GioHang
         public ActionResult XemGioHang()
         {
-            return View();
+            //Lấy giỏ hàng
+            List<ItemGioHang> lstGH = LayGioHang();
+
+            return View(lstGH);
         }
 
         public ActionResult GioHangPartial()
         {
+            if (TinhTongSoLuong() == 0)
+            {
+                ViewBag.TongSoLuong = 0;
+                ViewBag.TongTien = 0;
+                return PartialView();
+            }
+
+            ViewBag.TongSoLuong = TinhTongSoLuong();
+            ViewBag.TongTien = TinhTongTien();
+
             return PartialView();
         }
     }
---
+++ ThucChien/Models/ItemGioHang.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Web;
+
+namespace ThucChien.Models
+{
+    //View model, thực chất là 1 class bình thường được thêm vào, không phụ thuộc vào database model
+    public class ItemGioHang
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public int SoLuong { get; set; }
+        public decimal DonGia { get; set; }
+        public decimal ThanhTien { get; set; }
+        public string HinhAnh { get; set; }
+
+        public ItemGioHang(int iMaSP)
+        {
+            using (QuanLyBanHangEntities db = new QuanLyBanHangEntities())
+            {
+                this.MaSP = iMaSP;
+                SanPham sp = db.SanPhams.Single(n => n.MaSP == iMaSP);
+                this.TenSP = sp.TenSP;
+                this.HinhAnh = sp.HinhAnh;
+                this.DonGia = sp.DonGia.Value;
+                this.SoLuong = 1;
+                this.ThanhTien = DonGia * SoLuong;
+            }
+        }
+
+        public ItemGioHang(int iMaSP, int soLuong)
+        {
+            using (QuanLyBanHangEntities db = new QuanLyBanHangEntities())
+            {
+                this.MaSP = iMaSP;
+                SanPham sp = db.SanPhams.Single(n => n.MaSP == iMaSP);
+                this.TenSP = sp.TenSP;
+                this.HinhAnh = sp.HinhAnh;
+                this.DonGia = sp.DonGia.Value;
+                this.SoLuong = soLuong;
+                this.ThanhTien = DonGia* SoLuong;
+            }
+        }
+
+        public ItemGioHang()
+        {
+
+        }
+    }
+}
