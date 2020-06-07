24ef72c73175c545f4bdef824f50b99f3db5cf53
Le Duc Anh <leanhboi99@gmail.com>
2020-06-07 15:26:19 +0700

    Seach Ajax, Layout Admin

---
+++ ThucChien/Controllers/QuanLySanPhamController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThucChien.Models;
+
+namespace ThucChien.Controllers
+{
+    public class QuanLySanPhamController : Controller
+    {
+        private QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: QuanLySanPham
+        public ActionResult Index()
+        {
+            return View(db.SanPhams.Where(n => n.DaXoa == false));
+        }
+    }
+}
