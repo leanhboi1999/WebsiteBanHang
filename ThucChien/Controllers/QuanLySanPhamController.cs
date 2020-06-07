70be855e64e6be39df52ea6300121a4f0c07aba2
Le Duc Anh <leanhboi99@gmail.com>
2020-06-07 19:15:33 +0700

    Update trang dashboard tổng quát

--- ThucChien/Controllers/QuanLySanPhamController.cs
+++ ThucChien/Controllers/QuanLySanPhamController.cs
@@ -15,5 +15,30 @@ namespace ThucChien.Controllers
         {
             return View(db.SanPhams.Where(n => n.DaXoa == false));
         }
+
+        [HttpGet]
+        public ActionResult TaoMoi()
+        {
+            //Load dropdown nhà cung cấp, loại sản phẩm
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.MaNCC), "MaNCC", "TenNCC");
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.MaLoaiSP), "MaLoaiSP", "TenLoai");
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.MaNSX), "MaNSX", "TenNSX");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TaoMoi(HttpPostedFileBase HinhAnh)
+        {
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.OrderBy(n => n.MaNCC), "MaNCC", "TenNCC");
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams.OrderBy(n => n.MaLoaiSP), "MaLoaiSP", "TenLoai");
+            ViewBag.MaNSX = new SelectList(db.NhaSanXuats.OrderBy(n => n.MaNSX), "MaNSX", "TenNSX");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TaoMoi(FormCollection f, HttpPostedFileBase HinhAnh, string btn)
+        {
+            return View();
+        }
     }
 }
